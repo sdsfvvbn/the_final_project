94793dc82ee77bc7204db39f8eb823843dffe364
donut576 <sharon911023@gmai.com>
2025-05-11 22:56:17 +0800

    update test.py, models

--- myprofile/models.py
+++ myprofile/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-from  myprofile.models import Skill, Tag  # 替換 your_app_name 為實際應用名稱
 
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
