bfb494b16944be56de2fadebdbb1f7418787b849
donut576 <sharon911023@gmai.com>
2025-05-11 17:33:59 +0800

    Update views

--- myprofile/models.py
+++ myprofile/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from your_app_name.models import Skill, Tag  # 替換 your_app_name 為實際應用名稱
+from  myprofile.models import Skill, Tag  # 替換 your_app_name 為實際應用名稱
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
