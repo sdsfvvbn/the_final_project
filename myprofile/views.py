01d50c46b0f737cf763676c1014319b5f342852d
donut576 <sharon911023@gmai.com>
2025-05-11 17:37:36 +0800

    Update views

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from myprofile.models import UserProfile
 
 @login_required
 def profile_view(request):
