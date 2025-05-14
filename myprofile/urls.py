582d2f5ea81b51d67e41cfb41d28f4a5f92afaa4
donut576 <sharon911023@gmai.com>
2025-05-14 22:18:04 +0800

    create_profile

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile_view, name='profile_view'),  # 預設路徑為 /profile/
+    path('create/', views.create_profile, name='create_profile'),  # 定義 create_profile 路由
+    path('', views.profile_view, name='profile_view'),  # 定義 profile_view 路由
 ]
