62a5fd2c2eb312ba3b95ae28131a77e8ff5777f7
meisyalieandi65 <meisyalieandi65>
2025-05-19 11:36:57 +0800

    update 11:36 5/19

---
+++ homepage/static/homepage/scroll.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  const targets = document.querySelectorAll('.animate-fade');
+  targets.forEach(target => observer.observe(target));
+});
