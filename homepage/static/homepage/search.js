47fa505392d93fee71c5367bdc4bc02fde78f099
Weiting <hsuw005@gmail.com>
2025-05-20 20:35:23 +0800

    fix picture to filter

--- homepage/static/homepage/search.js
+++ homepage/static/homepage/search.js
@@ -1,8 +1,22 @@
-function redirectSearch() {
+function redirectSearch(category = '') {
     const input = document.getElementById("materialSearch");
-    const keyword = input.value.trim();
-
-    window.location.href = '/category/';
-
+    const keyword = input ? input.value.trim() : '';
+    
+    let url = '/category/';
+    const params = new URLSearchParams();
+    
+    if (category) {
+        params.append('category', category);
+    }
+    if (keyword) {
+        params.append('search', keyword);
+    }
+    
+    const queryString = params.toString();
+    if (queryString) {
+        url += '?' + queryString;
+    }
+    
+    window.location.href = url;
     return false;
 }
