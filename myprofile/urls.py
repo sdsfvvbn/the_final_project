b967aa096a8c61273a271d93b6fc426c5896b27a
donut576 <sharon911023@gmai.com>
2025-05-21 19:33:52 +0800

    new

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('create/', views.create_profile, name='create_profile'),  # 建立個人資料
-    path('edit/', views.edit_profile, name='edit_profile'),        # 編輯個人資料
-    path('', views.profile_view, name='profile_view'),             # 個人資料主頁
-    path('update-avatar/', views.update_avatar, name='update_avatar'),
+    path('create/', views.create_profile, name='create_profile'),      # 建立個人資料
+    path('edit/', views.edit_profile, name='edit_profile'),            # 編輯個人資料
+    path('update_avatar/', views.update_avatar, name='update_avatar'), # 上傳/更新頭像
+    path('', views.profile_view, name='profile_view'),                 # 個人資料主頁
 ]
